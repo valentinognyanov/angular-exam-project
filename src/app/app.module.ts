ba0284405ee09760252db90c827fd75a887f40ba
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-07-08 20:00:15 +0300

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+ 
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { GlobalLoaderComponent } from './global-loader/global-loader.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [GlobalLoaderComponent, NavigationComponent],
+  imports: [CommonModule, SharedModule, RouterModule],
+  exports: [GlobalLoaderComponent, NavigationComponent],
+})
+export class CoreModule {}
