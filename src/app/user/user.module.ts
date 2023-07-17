094c004b0aaa8f93483d66c4256f5830f3e1f39b
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-07-17 20:36:58 +0300

    create user and user-routing modules

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  imports: [CommonModule, UserRoutingModule],
+})
+export class UserModule {}
