735642932e3de09bec8cc72e8f9177fadf26e54e
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-07-23 16:17:49 +0300

    update login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { appEmailValidator } from 'src/app/shared/validators/app-email.validator';
+
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private fb: FormBuilder) {}
 
+  form = this.fb.group({
+    email: [
+      '',
+      [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)],
+    ],
+    password: ['', [Validators.required]],
+  });
+
+  login() {
+    console.log(this.form.value);
+  }
 }
