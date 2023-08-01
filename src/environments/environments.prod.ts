4df54e176ebf7c29984f6ffb5b32c8ea3d04dfb1
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-01 20:49:29 +0300

    update

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3030',
+  firebase: {
+    apiKey: 'AIzaSyDhl-9m1VvpTXyxRnIvIlzjgYgwXvvfMeI',
+    authDomain: 'angular-exam-project-87c3a.firebaseapp.com',
+    databaseURL:
+      'https://angular-exam-project-87c3a-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'angular-exam-project-87c3a',
+    storageBucket: 'angular-exam-project-87c3a.appspot.com',
+    messagingSenderId: '756857785341',
+    appId: '1:756857785341:web:ca1b5d9a977a9842986794',
+  },
 };
