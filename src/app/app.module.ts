a995f4b35b44e324ea0eb97399e4f17996186a36
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-01 20:48:56 +0300

    install firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
@@ -10,6 +12,8 @@ import { UserModule } from './user/user.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
@@ -20,6 +24,8 @@ import { AboutComponent } from './about/about.component';
     CoreModule,
     UserModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
 
   providers: [],
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-exam-project-87c3a',
+    appId: '1:756857785341:web:ca1b5d9a977a9842986794',
+    databaseURL:
+      'https://angular-exam-project-87c3a-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'angular-exam-project-87c3a.appspot.com',
+    apiKey: 'AIzaSyDhl-9m1VvpTXyxRnIvIlzjgYgwXvvfMeI',
+    authDomain: 'angular-exam-project-87c3a.firebaseapp.com',
+    messagingSenderId: '756857785341',
+  },
+};
