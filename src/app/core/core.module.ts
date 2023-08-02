4c1ebf6bda5b9e1a537de6266f52f6cd964e9759
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-02 20:49:11 +0300

    update navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
+import { UserModule } from '../user/user.module';
+
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [GlobalLoaderComponent, NavigationComponent, FooterComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, UserModule],
   exports: [GlobalLoaderComponent, NavigationComponent, FooterComponent],
 })
 export class CoreModule {}
