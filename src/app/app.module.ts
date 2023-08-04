62fe85aaf133a2e08dc9b06b31076db0e6ad9ae9
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-04 20:49:06 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { NewsModule } from './news/news.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
+import { UserService } from './user/user.service';
+
 import { environment } from '../environments/environment';
+import { NewsComponent } from './news/news/news.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent, NewsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
     UserModule,
+    NewsModule,
     BrowserAnimationsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
 
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
