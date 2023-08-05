e9eab3261923b432515f7e397c04f2beb03665ec
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-05 17:47:16 +0300

    update news component

--- src/app/news/news/news.component.ts
+++ src/app/news/news/news.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { NewsService } from '../news.service';
+
+import { News } from 'src/app/types/news';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
 })
 export class NewsComponent {
+  news!: News[];
 
+  constructor(private newsService: NewsService) {
+    this.newsService.getAllNews().subscribe((res) => {
+      this.news = res.map((n) => {
+        return {
+          id: n.payload.doc.id,
+          ...(n.payload.doc.data() as {}),
+        } as News;
+      });
+    });
+  }
 }
