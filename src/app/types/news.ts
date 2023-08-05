43a271b2ba8a579893922efe5249fbed5d4107a9
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-05 17:46:56 +0300

    create news model

---
+++ src/app/types/news.ts
@@ -0,0 +1,11 @@
+export interface News {
+  id: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  author: string;
+  authorId: string;
+  upvotes: any;
+  downvotes: any;
+  published: Date;
+}
