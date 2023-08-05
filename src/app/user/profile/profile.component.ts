1cdcf892f6b8a37000ccef71c2128543b2334060
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-05 17:47:50 +0300

    update profile component

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,20 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
-import { UserService } from '../user.service';
-import { User } from 'src/app/types/user.js';
+import { NewsService } from 'src/app/news/news.service';
+
+import { News } from 'src/app/types/news';
+
+import { USER_KEY } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
-  constructor(private userService: UserService) {}
+export class ProfileComponent {
+  news!: News[];
+  authorEmail = JSON.parse(localStorage.getItem(USER_KEY) as any).email;
+  routeUser: any;
+  users!: any;
 
-  user: any = undefined;
+  constructor(
+    public newsService: NewsService,
+    public angularFirestore: AngularFirestore,
+    private route: ActivatedRoute
+  ) {
+    this.newsService.getAllNews().subscribe((res) => {
+      this.news = res.map((p) => {
+        return {
+          id: p.payload.doc.id,
+          ...(p.payload.doc.data() as {}),
+        } as News;
+      });
+    });
 
-  ngOnInit(): void {
-    this.user = this.userService.getUser();
-    // console.log(this.user);
+    this.angularFirestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((users) => {
+        this.users = users;
+      });
+
+    const userId: any = this.route.parent?.snapshot.paramMap.get('id');
+    debugger;
+
+    this.angularFirestore
+      .collection('users')
+      .doc(userId)
+      .valueChanges()
+      .subscribe((user) => (this.routeUser = user));
+  }
+
+  get currentUser() {
+    return JSON.parse(localStorage.getItem(USER_KEY) as any);
   }
 }
