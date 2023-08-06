75c05ea50a79088b9335d738f1af021ba9888b65
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-06 20:11:34 +0300

    update navigation

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -5,14 +5,37 @@ import { UserService } from 'src/app/user/user.service';
 
 import { User } from 'src/app/types/user.model';
 
+import { USER_KEY } from 'src/app/shared/constants';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
-  userId: string;
-  constructor(public userService: UserService) {
-    this.userId = JSON.parse(userService.getUser)?.uid;
+  users: any;
+  
+  get isLogged(): boolean {
+    return !!localStorage.getItem(USER_KEY);
+  }
+
+  get userEmail() {
+    return JSON.parse(localStorage.getItem(USER_KEY) as any).email;
+  }
+  constructor(
+    public angularFirestore: AngularFirestore,
+    public userService: UserService
+  ) {
+    this.angularFirestore
+      .collection('users')
+      .snapshotChanges()
+      .subscribe((res) => {
+        this.users = res.map((n) => {
+          return {
+            id: n.payload.doc.id,
+            ...(n.payload.doc.data() as {}),
+          } as User;
+        });
+      });
   }
 }
