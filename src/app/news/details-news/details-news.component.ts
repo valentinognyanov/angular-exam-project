22e2b4fc42f75ece4e088c15eb4edddc16cc45e9
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-06 20:11:57 +0300

    update details component

--- src/app/news/details-news/details-news.component.ts
+++ src/app/news/details-news/details-news.component.ts
@@ -107,7 +107,7 @@ export class DetailsNewsComponent {
           }
         }
       });
-      this.router.navigate(['']);
+      this.router.navigate(['news']);
 
       return this.newsService.deleteNews(newsId);
     }
