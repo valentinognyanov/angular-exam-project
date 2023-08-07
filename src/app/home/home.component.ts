a8a1347b90b71f7eab8ac551d88f263d39b2f2f3
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-08-07 20:51:03 +0300

    home component update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { NewsService } from '../news/news.service';
+
+import { News } from '../types/news.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
